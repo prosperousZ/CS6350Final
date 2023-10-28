adc96270d42acf40d8712ed656c17b59d8929fad
Haoze Zhang <zhz1031108486@gmail.com>
2023-10-27 18:25:32 -0600

    Update try_to_plot_Euclid_vs_Was.py

--- 6350_project/try_to_plot_Euclid_vs_Was.py
+++ 6350_project/try_to_plot_Euclid_vs_Was.py
@@ -7,6 +7,7 @@
 import numpy as np
 import matplotlib.pylab as pl
 import ot
+import math
 from ot.datasets import make_1D_gauss as gauss
 
 #parameters
@@ -48,44 +49,28 @@ pl.plot(x, B[:, -1], 'b', label='Target distributions')
 pl.title('Target distributions')
 pl.tight_layout()
 
-# Compute EMD for the different losses
-# Compute and plot distributions and loss matrix
 
 
-#************This is where compute W1 Wasserstein distance
-d_emd = ot.emd2(a, B, M)  # direct computation of OT loss
-#********end computer W1**********
 
+
+#Euclidean distance
+
+
+d_emd = ot.emd2(a, B,M)
 #********This is where computer W2, M2 is square root of M1********
-d_emd2 = ot.emd2(a, B, M2)  # direct computation of OT loss with metric M2
+d_emd2 = ot.emd2(a, B, M2) 
 #*************end computer W2**************
 
 d_tv = [np.sum(abs(a - B[:, i])) for i in range(n_target)]
 
-pl.figure(2)
-pl.subplot(2, 1, 1)
-pl.plot(x, a, 'r', label='Source distribution')
-pl.title('Distributions')
-for i in range(n_target):
-    pl.plot(x, B[:, i], 'b', alpha=i / n_target)
-pl.plot(x, B[:, -1], 'b', label='Target distributions')
-pl.ylim((-.01, 0.13))
-pl.xticks(())
-pl.legend()
-pl.subplot(2, 1, 2)
-pl.plot(d_emd, label='Euclidean OT')
-pl.plot(d_emd2, label='Squared Euclidean OT')
-pl.plot(d_tv, label='Total Variation (TV)')
-pl.xlabel('Displacement')
-pl.title('Divergences')
-pl.legend()
-
 
 #Try to plot Euclidean distance vs Was distance
-pl.figure(3)
+pl.figure(2)
 pl.clf()
-pl.plot(M)
-pl.plot(d_emd, label='Euclidean OT')
+pl.plot(d_emd, label = 'Euclidean distance')
+pl.plot(d_emd2, label='Wasserstein distance')
+
+pl.xlabel('Displacement')
 
 pl.legend()
 
